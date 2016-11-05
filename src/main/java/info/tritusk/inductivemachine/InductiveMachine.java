f6f90249047de0465cdf204adbb17c096060abc0
3TUSK <urey.s.knowledge@gmail.com>
2016-11-05 16:10:29 -0400

    Separate blocks into a new tab

--- src/main/java/info/tritusk/inductivemachine/InductiveMachine.java
+++ src/main/java/info/tritusk/inductivemachine/InductiveMachine.java
@@ -3,6 +3,8 @@ package info.tritusk.inductivemachine;
 import ic2.api.event.TeBlockFinalCallEvent;
 import ic2.core.block.TeBlockRegistry;
 import info.tritusk.inductivemachine.block.BlockInductional;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
@@ -25,7 +27,12 @@ public class InductiveMachine {
 	
 	@Mod.EventHandler
 	public void postInit(FMLPostInitializationEvent event) {
-		//TODO Default recipe for laziness
+		TeBlockRegistry.get(BlockInductional.ID).setCreativeTab(new CreativeTabs("inductive_machine") {
+			@Override
+			public Item getTabIconItem() {
+				return Item.getItemFromBlock(TeBlockRegistry.get(BlockInductional.ID));
+			}		
+		});
 	}
 
 }
